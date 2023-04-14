b36f8e20efe9a47a976ebfee65415a0c5863775c
sllozier <sarah.lozier@gmail.com>
2023-04-14 11:50:25 -0400

    reame updated

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# Development Boilerplate
+# Personal Portfolio Website
 
-## Getting started
+[!view-img](https://sllozier.com/piccies/hero-image.png)
 
-1. Fork and clone this repo.
-2. `npm install`.
-3. `npm run start`
-4. name your database 
-## Details
-
-This is meant for development of a front-end, back-end project. Change .idea files to reflect project name. Change database.js to reflect proper database name. 
+You can click on this link to view my [portfolio](www.sllozier.com)!
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,47 +1,46 @@
 import React from "react";
 
-import Typewriter from 'typewriter-effect';
-
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
-
-    return (
-      <>
-        <section className="hero is-white has-text-centered is-fullheight">
-  <div className="hero-body">
-    <div className="container has-text-centered">
-        <div className="columns is-centered">
-            <div className="column">
-            <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">
-                Sarah Lozier</h1>
-                <h2 className="subtitle  is-size-4-tablet"><Typewriter
+  return (
+    <>
+      <section className="hero is-white has-text-centered is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div className="columns is-centered">
+              <div className="column">
+                <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">
+                  Sarah Lozier
+                </h1>
+                <h2 className="subtitle  is-size-4-tablet">
+                  <Typewriter
                     onInit={(typewriter) => {
-                        typewriter
+                      typewriter
                         .typeString("Full Stack Web Developer")
                         .pauseFor(1000)
                         .deleteAll()
-                        .typeString('I build cool things.')
+                        .typeString("I build cool things.")
                         .start();
-                    }}/>
+                    }}
+                  />
                 </h2>
-                <img className="avatar" src="/piccies/sarahAvatar.png" alt=""/>
+                <img className="avatar" src="/piccies/sarahAvatar.png" alt="" />
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-  </div>
 
- 
-  <div className="hero-foot">
-    <nav className="tabs is-boxed is-fullwidth">
-      <div className="container">
-        <img className="is-bottom" src="/piccies/hero-image.png" alt=""/>
-      </div>
-    </nav>
-  </div>
-</section>
-
-</>
-    )
+        <div className="hero-foot">
+          <nav className="tabs is-boxed is-fullwidth">
+            <div className="container">
+              <img className="is-bottom" src="/piccies/hero-image.png" alt="" />
+            </div>
+          </nav>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
